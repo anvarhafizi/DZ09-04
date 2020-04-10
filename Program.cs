f44ff7a65fee532cdabf91dbb1fbfe8627129906
unknown <yourGitHub@example.com>
2020-04-10 19:40:07 +0500

    Added Classes(DocumentWorker,ProDocumentWorker,ExpertWorker)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿	using System;
 
 namespace ConsoleAplicaton
 {
-    class Program
+    class Allclasses
     {
         static void Main(string[] args)
         {
-            
+           
+        }   
+        class DocumentWorker
+        {
+        public void OpenDocument()
+        {
+            Console.WriteLine("Open Document");
+        }
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Document editing is available in the Pro version.");
+        }
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Saving a document is available in the Pro version.");
+        }
+       
+        }
+        class ProDocumentWorker:DocumentWorker
+        {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Document edited");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("The document is saved in the old format, saving in other formats is available in the Expert version");
+        }
+        
+        }
+         class ExpertDocumentWorker:ProDocumentWorker
+        {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("The document is saved in a new format");
+        }
         }
     }
 }
